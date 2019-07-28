4479c3a2ac565189dd108f8642490ff975503f34
Harsha Meerchandani <Harsha.Meerchandani@in.ey.com>
2019-07-28 20:15:31 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -5,24 +5,51 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-
+    
 
 
     {/* &&&&&&&&&&&&&&&&&&     code here  &&&&&&&&&&&&&&&&&& */}
       <header className="App-header">
+      <div className="container">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Harsha</h1>
         <h2 className="second-heading">I am a full stack developer</h2>
+        </div>
       </header>
       <section>
-        
+        <div class="secmid">
+        <div className="container">
+      <div className="secleft">
+      <ul>
+        <li>Current Company:</li>
+        <li>Total experience:</li>
+        <li>Key-skills:</li>
+        <li>Projects:</li>
+      </ul>
+      </div>
+       <div className="secright">
+       <ul>
+        <li>Unilever</li>
+        <li>Total experience</li>
+        <li>Keyskills</li>
+        <li>Details</li>
+      </ul>
+       </div>
+       </div>
+       </div>
       </section>
       <footer>
-  
+      <div className="container"> 
+      <div className="contact-details">
+      <p><span>Full Name</span><span>Harsha Meerchandani</span></p>
+      <p><span>Email Id</span><span>Harsha1234d@gmail.com</span></p>
+      <p><span>Mobile Number</span><span>9770385858</span></p>
+      </div>
+      </div>
       </footer>
 
     {/* &&&&&&&&&&&&&&&&&&     end code here  &&&&&&&&&&&&&&&&&& */}
-
+  
     </div>
   );
 }
