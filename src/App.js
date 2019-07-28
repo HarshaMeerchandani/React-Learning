0baa531c765e68b149daaf736d7273bca75346c8
Harsha Meerchandani <Harsha.Meerchandani@in.ey.com>
2019-07-28 12:09:41 +0530

    First Commit- Personal Website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+
+
+
+    {/* &&&&&&&&&&&&&&&&&&     code here  &&&&&&&&&&&&&&&&&& */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Harsha</h1>
+        <h2 className="second-heading">I am a full stack developer</h2>
       </header>
+      <section>
+        
+      </section>
+
+    {/* &&&&&&&&&&&&&&&&&&     end code here  &&&&&&&&&&&&&&&&&& */}
+
     </div>
   );
 }
